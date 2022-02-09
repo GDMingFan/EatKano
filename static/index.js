ed738b57cd027a222febbb8cbfd3651e41470941
GDMingFan <99320501+GDMingFan@users.noreply.github.com>
2022-02-09 17:12:14 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,11 +441,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
-        return '人？';
+        if (cps <= 5) return '觉撒';
+        if (cps <= 8) return '觉撒列';
+        if (cps <= 10)  return '死咧';
+        if (cps <= 15) return '揍吊咧';
+        return '蒸的？';
     }
 
     function toStr(obj) {
